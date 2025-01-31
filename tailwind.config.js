e42d835bdaf950d4edbceacc8ad38ed8539c516b
Kelly Owoju <kelliebrighty@gmail.com>
2025-01-31 01:19:57 +0100

    pushing updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
+
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    plugin(function ({ addUtilities }) {
-      addUtilities({
-        ".scrollbar-hide": {
-          "-ms-overflow-style": "none",
-
-          /* Firefox */
-          "scrollbar-width": "none",
-
-          /* Safari and Chrome */
-          "&::-webkit-scrollbar": {
-            display: "none",
-          },
-        },
-      });
-    }),
-  ],
+  plugins: [],
 };
